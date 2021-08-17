373cfcaebe515a987cba496a8f5424284c2aee35
cacagdas <alicagdascolak@gmail.com>
2021-08-17 13:32:21 +0300

    creating todos cubit

---
+++ lib/cubit/todos_state.dart
@@ -0,0 +1,6 @@
+part of 'todos_cubit.dart';
+
+@immutable
+abstract class TodosState {}
+
+class TodosInitial extends TodosState {}
--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_todo_app/constants/strings.dart';
+import 'package:flutter_todo_app/cubit/todos_cubit.dart';
 import 'package:flutter_todo_app/presentation/pages/add_todo_page.dart';
 import 'package:flutter_todo_app/presentation/pages/edit_todo_page.dart';
 import 'package:flutter_todo_app/presentation/pages/todo_page.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
-    switch(settings.name) {
+    switch (settings.name) {
       case "/":
-        return MaterialPageRoute(builder: (_) => TodosPage());
+        return MaterialPageRoute(builder: (_) =>
+            BlocProvider(
+              create: (context) => TodosCubit(),
+              child: TodosPage(),
+            ));
       case EDIT_TODO_ROUTE:
         return MaterialPageRoute(builder: (_) => EditTodoPage());
       case ADD_TODO_ROUTE:
